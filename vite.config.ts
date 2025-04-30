86cffb3741b40ae8cbdde7762505bda2549e367f
Yesahk Ergano <yesahkergano@Yesahks-Air.lan>
2025-04-29 22:06:01 -0400

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yesahk-movie",
+
   plugins: [react()],
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
+    
+    
   },
 });
